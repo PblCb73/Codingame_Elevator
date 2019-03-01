c2ca213e7a82f3b8966af448d63972279d162454
test <test@test.com>
2019-03-01 23:00:19 +0400

    remove all and add new solution

--- main.cpp
+++ main.cpp
@@ -3,45 +3,17 @@
 
 using namespace std;
 
-/**
- * Auto-generated code below aims at helping you parse
- * the standard input according to the problem statement.
- **/
 int main()
 {
-    int n;
-    int a;
-    int b;
-    int k;
-    int m;
-    cin >> n >> a >> b >> k >> m; cin.ignore();    
-    
-    bool flagImpossible( false );//Флаг того, что приехать на указанный этаж невозможно
-    int pressCount( 0 ); //Количество нажатий
-    int lastDownFloor( 0 ); //Последний этаж куда мы спускались
-    
-    //Поехали
-    while(  k != m && !flagImpossible )
-    {
-        //Если лифт выше нужного этажа, будем опускаться пока не приедем на этаж или не проедем
-        while(  k > m ){
-             k =  k - b;
-            pressCount++;
+    int n, a, b, k, m;    
+    cin >> n >> a >> b >> k >> m; cin.ignore();
+	//Решим уравнение методом подбора с условиями. Возьмем первое решение, так как может быть множество решений!
+    for( int x( 0 ) ; x < n - a ; ++x ){
+        if( !( ( a * x  - ( m - k ) ) % b ) ){
+            cout << x + abs( a * x - ( m - k )) / b;
+            return 0;
         }
-        //Посмотрим, были ли мы на этом этаже уже, если мы были, то мы зациклились
-        if(  k == lastDownFloor ) flagImpossible = true;
-        else lastDownFloor =  k;
-        //Если лифт ниже нужного этажа, будем подниматься пока не приедем на этаж или не проедем
-        while(  k < m ){
-             k =  k + a;
-            pressCount++;
-        }
-        //Если лифт улетел на чердак или в подвал, это провал...
-        if(  k > n ||  k < 1 ) flagImpossible = true;
     }
-
-    if( flagImpossible )
-        cout << "IMPOSSIBLE" << endl;
-    else
-        cout << pressCount << endl;    
+    cout  << "IMPOSSIBLE";
+    return 0;
 }
