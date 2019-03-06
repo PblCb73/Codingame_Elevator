6c28c8c06a1763f6fd6d41806af692a78e9091a7
test <test@test.com>
2019-03-06 22:40:37 +0400

    New fix of the condition

--- main.cpp
+++ main.cpp
@@ -8,9 +8,10 @@ int main()
     int n, a, b, k, m;    
     cin >> n >> a >> b >> k >> m; cin.ignore();
 	//Решим уравнение методом подбора с условиями. Возьмем первое решение, так как может быть множество решений!
-    for( int x( 0 ) ; x < n - a ; ++x ){
+    for( int x( 0 ) ; x < n ; ++x ){
         int y( abs( a * x - ( m - k ) ) / b );
-        if( k + ( a * x - y * b ) == m ){
+        int d( a * x - y *b );
+        if( d + a < n && k + d == m ){
             cout << x + y;
             return 0;
         }
