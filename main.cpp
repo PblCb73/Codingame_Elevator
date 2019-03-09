a2f9557c6fd5a042646b4362a87d7e29b083a36b
test <test@test.com>
2019-03-09 16:57:44 +0400

    Returned to the algorithm with if else, but modified

--- main.cpp
+++ main.cpp
@@ -1,21 +1,31 @@
 #include <iostream>
 #include <string>
+#include <set>
 
 using namespace std;
 
 int main()
 {
-    int n, a, b, k, m;    
-    cin >> n >> a >> b >> k >> m; cin.ignore();
-	//Решим уравнение методом подбора с условиями. Возьмем первое решение, так как может быть множество решений!
-    for( int x( 0 ) ; x < n ; ++x ){
-        int y( abs( a * x - ( m - k ) ) / b );
-        int d( a * x - y *b );
-        if( d + a < n && k + d == m ){
-            cout << x + y;
-            return 0;
-        }
-    }
-    cout  << "IMPOSSIBLE";
-    return 0;
+	int n;
+	int a;
+	int b;
+	int k;
+	int m;
+	cin >> n >> a >> b >> k >> m; cin.ignore();
+	set<int> floors;
+	//Если мы были уже на текущем этаже, значит решения нет
+	while( floors.find( k ) == floors.end() ){
+		floors.insert( k );//Добавим текущий этаж во множество этажей, на которых мы были
+		//Важен порядок проверки
+		if( ( k < m ) && ( k + a <= n ) ) k += a;//Текущий этаж ниже чем требуемый, и можно двигаться на верх
+		else if( ( k < m ) && ( k - b > 0 ) ) k -= b;//Текущий этаж ниже чем требуемый, и можно двигаться вниз
+		else if( ( k > m ) && ( k - b > 0 ) ) k -= b;//Текущий этаж выше чем требуемый, и можно двигаться вниз
+		else if( ( k > m ) && (  k + a <= n ) ) k += a;//Текущий этаж выше чем требуемый, и можно двигаться на верх
+		if( k == m ){ //Если нашли, то выводим результат, и выходим из цикла
+			cout << floors.size();
+			return 0;
+		}
+	}
+	cout<<"IMPOSSIBLE";
+	return 0;
 }
